79d55e3f0d4ec337686cc3809a7049853d82c389
Kyle Kelley <rgbkrk@gmail.com>
2018-11-05 17:21:40 -0800

    more code 26527

--- index.js
+++ index.js
@@ -1,24 +1,59 @@
-const { ApolloServer } = require('apollo-server')
-
+// @format
+const {ApolloServer} = require('apollo-server');
 
 const typeDefs = `
+	# 1. Add Photo type definition
+	type Photo {
+	  id: ID!
+		url: String!
+		name: String!
+		description: String
+	}
+
+  # 2. Return Photo from allPhotos
   type Query {
 	  totalPhotos: Int!
+		allPhotos: [Photo!]!
 	}
-`
+
+  # 3. Return the newly posted photo from the mutation
+	type Mutation {
+	  postPhoto(name: String! description: String): Photo!
+	}
+`;
+
+// HACK: variable we increment to make unique ids
+var _id = 0;
+
+var photos = [];
 
 const resolvers = {
   Query: {
-	  totalPhotos: () => 42
-  }
-}
+    totalPhotos: () => photos.length,
+    allPhotos: () => photos,
+  },
+
+  Mutation: {
+    postPhoto(parent, args) {
+      var newPhoto = {
+        id: _id++,
+        ...args,
+      };
+      photos.push(newPhoto);
+
+      return newPhoto;
+    },
+  },
+  Photo: {
+    url: parent => `http://yoursite.com/img/${parent.id}.jpg`,
+  },
+};
 
 const server = new ApolloServer({
   typeDefs,
-	resolvers
-})
-
+  resolvers,
+});
 
 server
   .listen()
-	.then(({url}) => console.log(`GraphQL Service running on ${url}`))
+  .then(({url}) => console.log(`GraphQL Service running on ${url}`));
