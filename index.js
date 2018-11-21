203b96627becfee5f116f07f3ad1e2818def6eb9
Kyle Kelley <rgbkrk@gmail.com>
2018-11-21 11:20:51 -0800

    ability to tag photos, store creation date

--- index.js
+++ index.js
@@ -1,45 +1,55 @@
 // @format
-const { ApolloServer } = require("apollo-server");
+const { ApolloServer, gql } = require("apollo-server");
 
-const typeDefs = `
+const { GraphQLScalarType } = require("graphql");
+
+const typeDefs = gql`
+  scalar DateTime
+
   type User {
-	  githubLogin: ID!
-		name: String
-		avatar: String
-		postedPhotos: [Photo!]!
-	}
+    githubLogin: ID!
+    name: String
+    avatar: String
+    postedPhotos: [Photo!]!
+    inPhotos: [Photo!]!
+  }
 
   enum PhotoCategory {
-	  SELFIE
-		PORTRAIT
-		ACTION
-		LANDSCAPE
-		GRAPHIC
-	}
+    SELFIE
+    PORTRAIT
+    ACTION
+    LANDSCAPE
+    GRAPHIC
+  }
 
-	type Photo {
-	  id: ID!
-		url: String!
-		name: String!
-		description: String
-		category: PhotoCategory!
-		postedBy: User!
-	}
+  type Photo {
+    id: ID!
+    url: String!
+    name: String!
+    description: String
+    category: PhotoCategory!
+    postedBy: User!
+    taggedUsers: [User!]!
+    created: DateTime!
+  }
 
-	input PostPhotoInput {
-	  name: String!
-		category: PhotoCategory=PORTRAIT
-		description: String
-	}
+  # Left off ~ here, doing many-to-many on page 96
+
+  input PostPhotoInput {
+    name: String!
+    category: PhotoCategory = PORTRAIT
+    description: String
+  }
 
   type Query {
-	  totalPhotos: Int!
-		allPhotos: [Photo!]!
-	}
+    totalPhotos: Int!
+    allPhotos: [Photo!]!
+  }
 
-	type Mutation {
-	  postPhoto(input: PostPhotoInput!): Photo!
-	}
+  type Mutation {
+    postPhoto(input: PostPhotoInput!): Photo!
+  }
+
 `;
 
 // HACK: variable we increment to make unique ids
@@ -57,21 +67,31 @@ var photos = [
     name: "Dropping the Heart Chute",
     description: "The heart chute is one of my favorite chutes",
     category: "ACTION",
-    githubUser: "gPlake"
+    githubUser: "gPlake",
+    created: "3-28-1977"
   },
   {
     id: "2",
     name: "Enjoying the sunshine",
     category: "SELFIE",
-    githubUser: "sSchmidt"
+    githubUser: "sSchmidt",
+    created: "1-2-1985"
   },
   {
     id: "3",
     name: "Gunbarrel 25",
     description: "25 laps on gunbarrel today",
     category: "LANDSCAPE",
-    githubUser: "sSchmidt"
+    githubUser: "sSchmidt",
+    created: "2018-04-15T19:09:57.308Z"
   }
+];
+
+var tags = [
+  { photoID: "1", userID: "gPlake" },
+  { photoID: "2", userID: "sSchmidt" },
+  { photoID: "2", userID: "mHattrup" },
+  { photoID: "2", userID: "gPlake" }
 ];
 
 const resolvers = {
@@ -84,7 +104,8 @@ const resolvers = {
     postPhoto(parent, args) {
       var newPhoto = {
         id: _id++,
-        ...args.input
+        ...args.input,
+        created: new Date()
       };
       photos.push(newPhoto);
 
@@ -95,13 +116,40 @@ const resolvers = {
     url: parent => `http://yoursite.com/img/${parent.id}.jpg`,
     postedBy: parent => {
       return users.find(u => u.githubLogin === parent.githubUser);
-    }
+    },
+    taggedUsers: parent =>
+      tags
+        // Returns an array of tags that only contain the current photo
+
+        .filter(tag => tag.photoID === parent.id)
+        // Converts the array of tags into an array of userIDs
+        .map(tag => tag.userID)
+
+        // Converts array of userIDs into an array of user objects
+        .map(userID => users.find(u => u.githubLogin === userID))
   },
   User: {
     postedPhotos: parent => {
       return photos.filter(p => p.githubUser === parent.githubLogin);
-    }
-  }
+    },
+    inPhotos: parent =>
+      tags
+        // Returns an array of tags that only contain the current user
+        .filter(tag => tag.userID === parent.id)
+
+        // Converts the array of tags into an array of photoIDs
+        .map(tag => tag.photoID)
+
+        // Converts array of photoIDs into an array of photo objects
+        .map(photoID => photos.find(p => p.id === photoID))
+  },
+  DateTime: new GraphQLScalarType({
+    name: "DateTime",
+    description: "A valid date time value.",
+    parseValue: value => new Date(value),
+    serialize: value => new Date(value).toISOString(),
+    parseLiteral: ast => ast.value
+  })
 };
 
 const server = new ApolloServer({
