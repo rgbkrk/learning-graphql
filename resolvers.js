652dcdea2bcacfb120cb963b0002bb0adbde7fa0
Kyle Kelley <rgbkrk@gmail.com>
2018-11-24 17:34:13 -0800

    switch more ops to db backed

--- resolvers.js
+++ resolvers.js
@@ -2,52 +2,6 @@ const { GraphQLScalarType } = require("graphql");
 
 const { githubAuth } = require("./auth");
 
-var users = [
-  { githubLogin: "mHattrup", name: "Mike Hattrup" },
-  { githubLogin: "gPlake", name: "Glen Plake" },
-  { githubLogin: "sSchmidt", name: "Scot Schmidt" }
-];
-
-var photos = [
-  {
-    id: "1",
-    name: "Dropping the Heart Chute",
-    description: "The heart chute is one of my favorite chutes",
-    category: "ACTION",
-    githubUser: "gPlake",
-    created: "3-28-1977"
-  },
-  {
-    id: "2",
-    name: "Enjoying the sunshine",
-    category: "SELFIE",
-    githubUser: "sSchmidt",
-    created: "1-2-1985"
-  },
-  {
-    id: "3",
-    name: "Gunbarrel 25",
-    description: "25 laps on gunbarrel today",
-    category: "LANDSCAPE",
-    githubUser: "sSchmidt",
-    created: "2018-04-15T19:09:57.308Z"
-  }
-];
-
-// HACK: variable we increment to make unique ids in the postPhoto mutation
-var _id = photos
-  .map(photo => parseInt(photo.id))
-  // Find the largest id in our photos collection and increment by 1
-  .reduce((prev, current) => (current > prev ? current + 1 : prev), 0)
-  .toString();
-
-var tags = [
-  { photoID: "1", userID: "gPlake" },
-  { photoID: "2", userID: "sSchmidt" },
-  { photoID: "2", userID: "mHattrup" },
-  { photoID: "2", userID: "gPlake" }
-];
-
 const resolvers = {
   Query: {
     totalPhotos: (parent, args, { db }) =>
@@ -72,48 +26,40 @@ const resolvers = {
   },
 
   Mutation: {
-    postPhoto(parent, args) {
-      var newPhoto = {
-        id: _id++,
+    async postPhoto(parent, args, { db, currentUser }) {
+      // When there isn't a user in context, throw error
+      if (!currentUser) {
+        throw new Error("only an authorized user can post a photo");
+      }
+
+      const newPhoto = {
         ...args.input,
+        userID: currentUser.githubLogin,
         created: new Date()
       };
-      photos.push(newPhoto);
+
+      const result = await db.collection("photos").insertOne(newPhoto);
+
+      newPhoto.id = result.insertedId.toHexString();
 
       return newPhoto;
     },
     githubAuth
   },
   Photo: {
-    url: parent => `http://yoursite.com/img/${parent.id}.jpg`,
-    postedBy: parent => {
-      return users.find(u => u.githubLogin === parent.githubUser);
-    },
-    taggedUsers: parent =>
-      tags
-        // Returns an array of tags that only contain the current photo
-        .filter(tag => tag.photoID === parent.id)
+    id: parent => parent.id || parent._id.toString(),
+    url: parent => `/img/photos/${parent._id.toString()}.jpg`,
 
-        // Converts the array of tags into an array of userIDs
-        .map(tag => tag.userID)
-
-        // Converts array of userIDs into an array of user objects
-        .map(userID => users.find(u => u.githubLogin === userID))
+    postedBy: (parent, args, { db }) =>
+      db.collection("users").findOne({ githubLogin: parent.userID })
   },
   User: {
-    postedPhotos: parent => {
-      return photos.filter(p => p.githubUser === parent.githubLogin);
-    },
-    inPhotos: parent =>
-      tags
-        // Returns an array of tags that only contain the current user
-        .filter(tag => tag.userID === parent.id)
-
-        // Converts the array of tags into an array of photoIDs
-        .map(tag => tag.photoID)
-
-        // Converts array of photoIDs into an array of photo objects
-        .map(photoID => photos.find(p => p.id === photoID))
+    postedPhotos: (parent, args, { db }) => {
+      return db
+        .collection("photos")
+        .find({ userID: parent.githubLogin })
+        .toArray();
+    }
   },
   DateTime: new GraphQLScalarType({
     name: "DateTime",
