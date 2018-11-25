e5f8d300f1aff48e9e64d2dc901e7a297df025b3
Kyle Kelley <rgbkrk@gmail.com>
2018-11-24 19:17:30 -0800

    break out resolvers

--- resolvers/index.js
+++ resolvers/index.js
@@ -1,5 +1,7 @@
 const { GraphQLScalarType } = require("graphql");
 
+const fetch = require("node-fetch");
+
 const { githubAuth } = require("./auth");
 
 const resolvers = {
@@ -44,8 +46,39 @@ const resolvers = {
 
       return newPhoto;
     },
-    githubAuth
+    githubAuth,
+
+    async addFakeUsers(parent, { count }, { db }) {
+      var randomUserApi = `https://randomuser.me/api/?results=${count}`;
+
+      var { results } = await fetch(randomUserApi).then(res => res.json());
+
+      var users = results.map(r => ({
+        githubLogin: r.login.username,
+        name: `${r.name.first} ${r.name.last}`,
+        avatar: r.picture.thumbnail,
+        githubToken: r.login.sha1
+      }));
+
+      await db.collection("users").insert(users);
+
+      return users;
+    },
+
+    async fakeUserAuth(parent, { githubLogin }, { db }) {
+      var user = await db.collection("users").findOne({ githubLogin });
+
+      if (!user) {
+        throw new Error(`Cannot find user with githubLogin "${githubLogin}"`);
+      }
+
+      return {
+        token: user.githubToken,
+        user
+      };
+    }
   },
+
   Photo: {
     id: parent => parent.id || parent._id.toString(),
     url: parent => `/img/photos/${parent._id.toString()}.jpg`,
